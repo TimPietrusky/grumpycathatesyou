215dab42080236b2be108cac94e20d54879e90a3
Tim Pietrusky <timpietrusky@googlemail.com>
2013-03-24 17:51:51 +0100

    Initial commit.

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
   <meta name="author" content="Tim Pietrusky">
   <meta name="robots" content="index,follow">
   <meta name="revisit-after" content="1 days">
-  <meta name="description" content="@TODO">
+  <meta name="description" content="Create your own Grumpy Cat meme or die trying. I'm not kidding, asshole!">
 
   <meta property="og:title" content="Grumpy Cat hates you D:">
-  <meta property="og:description" content="@TODO">
+  <meta property="og:description" content="Create your own Grumpy Cat meme or die trying. I'm not kidding, asshole!">
   <meta property="og:image" content="http://grumpycathatesyou.com/img/grumpycathatesyou_200.jpg">
   <meta property="og:url" content="http://grumpycathatesyou.com">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
@@ -112,7 +112,8 @@
   </script>
   
   <?php if ($isLive): ?>
-    <script src="/js/scripts.js"></script>
+    <script src="/js/libs.js"></script>
+    <script src="/js/main-min.js"></script>
     <script type="text/javascript">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-5596313-9']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
   <?php else: ?>
     <script src="/js/libs.js"></script>
